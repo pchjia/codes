7297ec6963a457652b3c3a786f13351458d9b81c
pchjia <pchjia@sina.cn>
2015-12-14 22:49:04 +0800

    278-281

--- C/computer_system/277.c
+++ C/computer_system/277.c
@@ -2,5 +2,22 @@
 
 // divide by power of two. assume 0 <= k <= w-1
 int divide_power2(int x, int k){
-    
+    int w = sizeof(int) << 3;
+    int x_sign = x >> (w-1);
+    int x_carry = x & ((1<<k) - 1);
+    x >>= k;
+    x += x_sign && x_carry;
+    return x;
+}
+
+int main(int argc, const char *argv[])
+{
+    printf("%d", divide_power2(3, 1));
+    printf("\n");
+    printf("%d", divide_power2(-3, 1));
+    printf("\n");
+    printf("%d", divide_power2(4, 1));
+    printf("\n");
+    printf("%d", divide_power2(-4, 1));
+    return 0;
 }
