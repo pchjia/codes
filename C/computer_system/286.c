1eb7cc2ad4891b2736e3efdec57c5d406b304599
pchjia <pchjia@sina.cn>
2015-12-15 19:02:17 +0800

    homework 2.86

---
+++ C/computer_system/286.c
@@ -0,0 +1,9 @@
+/*
+ * desc                     Hex             M               E           V 
+ * -0                       0x8000          0               -62         --
+ * minimum number > 1       0x3f01          257/256         0           257 * 2^(-8)
+ * 256                      0x4700          1               8           --
+ * maximun non-normalized   0x00ff          255/256         -62         255 * 2^(-70)
+ * -inf                     0xff00          --              --          --
+ * 0x3AA0                   0x3aa0          416/256         -5          416 * 2^(-13)
+ */
