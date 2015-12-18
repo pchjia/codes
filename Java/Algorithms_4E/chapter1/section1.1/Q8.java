e4c12178ec2906b9c410d03be922f93bad3c3715
pchjia <pchjia@sina.cn>
2015-12-18 21:42:54 +0800

    practice 1.1.8

---
+++ Java/Algorithms_4E/chapter1/section1.1/Q8.java
@@ -0,0 +1,11 @@
+public class Q8{
+    /*  a. System.out.println('b'); : b
+     *  b. System.out.println('b' + 'c'); : 197
+     *  c. System.out.println((char)('a' + 4)); : e
+     */
+    public static void main (String[] args) {
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char)('a' + 4));
+    }
+}
