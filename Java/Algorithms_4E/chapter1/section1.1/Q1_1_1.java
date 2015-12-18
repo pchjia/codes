268542000e4a5f9e7c0a641c2418ecb97919d8e1
pchjia <pchjia@sina.cn>
2015-12-18 20:32:37 +0800

    practice 1.1.1

---
+++ Java/Algorithms_4E/chapter1/section1.1/Q1_1_1.java
@@ -0,0 +1,12 @@
+public class Q1_1_1{
+    /*
+     * a. (0 + 15) / 2: 7
+     * b. 2.0e-6 * 100000000.1: 200.0000002
+     * c. true && false || true && true: true
+     */
+    public static void main (String[] args) {
+        StdOut.println((0 + 15) / 2);
+        StdOut.println(2.0e-6 * 100000000.1);
+        StdOut.println(true && false || true && true);
+    }
+}
