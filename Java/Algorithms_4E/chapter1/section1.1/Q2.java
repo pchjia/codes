f588b5750dd7b7f0c50ca48a9a0883ba0ecde4e9
pchjia <pchjia@sina.cn>
2015-12-18 20:38:03 +0800

    practice 1.1.2

---
+++ Java/Algorithms_4E/chapter1/section1.1/Q2.java
@@ -0,0 +1,14 @@
+public class Q2{
+    /*
+     * a. (1 + 2.36) / 2: double, 1.68
+     * b. 1 + 2 + 3 + 4.0: double, 10.0
+     * c. 4.1 >= 4: boolean, true
+     * d. 1 + 1 + "3": String, 23
+     */
+    public static void main (String[] args) {
+        StdOut.println((1 + 2.36) / 2);
+        StdOut.println(1 + 2 + 3 + 4.0);
+        StdOut.println(4.1 >= 4);
+        StdOut.println(1 + 1 + "3");
+    }
+}
