1ff3723f86592ae972d67e9d7c627adc3f36938c
pchjia <pchjia@sina.cn>
2015-12-23 21:00:55 +0800

    Counter class

---
+++ Java/Algorithms_4E/chapter1/Counter.java
@@ -0,0 +1,13 @@
+public class Counter{
+    // construction method
+    Counter(String id){};
+
+    // increate count by 1
+    public void increment(){}
+
+    // times that the object has been increated
+    public int tally(){};
+
+    // parse Object to String
+    public String toString(){}
+}
