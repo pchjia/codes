f933af248b6f8766c7b7ae106adafde48bce6c9e
pchjia <pchjia@sina.cn>
2015-12-30 22:02:38 +0800

    Point2D

--- Java/Algorithms_4E/chapter1/section1.2/Point2D.java
+++ Java/Algorithms_4E/chapter1/section1.2/Point2D.java
@@ -26,10 +26,6 @@ public class Point2D {
     }
 
     public void draw() {
-        StdDraw.setXscale(-2*Math.abs(x), 2*Math.abs(x));
-        StdDraw.setYscale(-2*Math.abs(y), 2*Math.abs(y));
-        StdDraw.setPenRadius(.005);
-
         StdDraw.point(x(), y());
     }
 }
--- Java/Algorithms_4E/chapter1/section1.2/Q1.java
+++ Java/Algorithms_4E/chapter1/section1.2/Q1.java
@@ -2,5 +2,8 @@ public class Q1 {
     public static void main (String[] args) {
         int N = Integer.parseInt(args[0]);
 
+        for (int i=0; i<N; i++) {
+            
+        }
     }
 }
