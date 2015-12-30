6587c74f5cbe188440de516d4eabe09fcf415e5c
pchjia <pchjia@sina.cn>
2015-12-31 00:08:25 +0800

    Q1

--- Java/Algorithms_4E/chapter1/section1.2/Q1.java
+++ Java/Algorithms_4E/chapter1/section1.2/Q1.java
@@ -1,9 +1,15 @@
 public class Q1 {
     public static void main (String[] args) {
         int N = Integer.parseInt(args[0]);
+        Point2D[] points = new Point2D[N];
+
+        StdDraw.setXscale(-1.0, 1.0);
+        StdDraw.setYscale(-1.0, 1.0);
+        StdDraw.setPenRadius(0.005);
 
         for (int i=0; i<N; i++) {
-            
+            points[i] = new Point2D(StdRandom.uniform(-1.0, 1.0), StdRandom.uniform(-1.0, 1.0));
+            points[i].draw();
         }
     }
 }
