908983d9d815a7ef614e5e6ced8baf06c6dd3c14
pchjia <pchjia@sina.cn>
2015-12-31 10:10:52 +0800

    practise 1.2.1

--- Java/Algorithms_4E/chapter1/section1.2/Q1.java
+++ Java/Algorithms_4E/chapter1/section1.2/Q1.java
@@ -7,9 +7,21 @@ public class Q1 {
         StdDraw.setYscale(-1.0, 1.0);
         StdDraw.setPenRadius(0.005);
 
+        double min = 2.0;
         for (int i=0; i<N; i++) {
             points[i] = new Point2D(StdRandom.uniform(-1.0, 1.0), StdRandom.uniform(-1.0, 1.0));
             points[i].draw();
+            for (int j=0; j<i; j++) {
+                double xth = points[i].x() - points[j].x();
+                double yth = points[i].y() - points[j].y();
+                double dist = xth*xth + yth*yth;
+                if (dist < min) {
+                    min = dist;
+                }
+            }
         }
+
+        min = Math.sqrt(min);
+        System.out.println(min);
     }
 }
