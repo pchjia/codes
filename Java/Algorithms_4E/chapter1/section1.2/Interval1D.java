b86b2fb09fff21446a9ab3109f6ef24c291263e8
pchjia <pchjia@sina.cn>
2015-12-31 10:18:08 +0800

    Interval1D

---
+++ Java/Algorithms_4E/chapter1/section1.2/Interval1D.java
@@ -0,0 +1,33 @@
+public class Interval1D {
+    private double lo;
+    private double hi;
+
+    public Interval1D(double lo, double hi) {
+        this.lo = lo;
+        this.hi = hi;
+    }
+    
+    public double length() {
+        return hi - lo;
+    }
+
+    public boolean contains(double x) {
+        if (x < lo || x > hi) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean intersect(Interval1D that) {
+        if (this.lo > that.hi || that.lo > this.hi) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void draw() {
+        StdDraw.line(0.0, lo, 0.0, hi);
+    }
+}
