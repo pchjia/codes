aa711dc523113494cd6a5252e8a4835a4233a5e1
pchjia <pchjia@sina.cn>
2016-01-01 12:23:50 +0800

    improvement 1.2.15

---
+++ Java/Algorithms_4E/chapter1/section1.2/Q15.java
@@ -0,0 +1,16 @@
+public class Q15 {
+    public static void main (String[] args) {
+        
+    }
+
+    public static int[] readInts(String name) {
+        In in = new In(name);
+        String input = in.readAll();
+        String[] words = input.split("\\s+");
+        int[] ints = new int[words.length];
+        for (int i=0; i<words.length; i++) {
+            ints[i] = Integer.parseInt(words[i]);
+        }
+        return ints;
+    }
+}
