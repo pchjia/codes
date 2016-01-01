bc4bc7fe455d2c3675c9c6676b8349652885b9c1
pchjia <pchjia@sina.cn>
2016-01-01 20:09:16 +0800

    FixedCapacityStack

---
+++ Java/Algorithms_4E/chapter1/FixedCapacityStack.java
@@ -0,0 +1,25 @@
+public class FixedCapacityStack<Item> {
+    private Item[] a;
+    private int N;
+
+    @SuppressWarnings("unchecked")
+    public FixedCapacityStack(int cap) {
+        a = (Item[]) new Object[cap];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        a[N++] = item;
+    }
+
+    public Item pop() {
+        return a[--N];
+    }
+}
