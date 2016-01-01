d72a4c31dc7ca37661cb414cb5af4fefe259813e
pchjia <pchjia@sina.cn>
2016-01-01 09:17:10 +0800

    practise 1.2.4

---
+++ Java/Algorithms_4E/chapter1/section1.2/Q4.java
@@ -0,0 +1,17 @@
+/*
+ * String string1 = "hello";
+ * String string2 = string1;
+ * string1 = "world";
+ * StdOut.println(string1);         // wordl
+ * StdOut.println(string2);         // hello
+ */
+
+public class Q4 {
+    public static void main (String[] args) {
+        String string1 = "hello";
+        String string2 = string1;
+        string1 = "world";
+        StdOut.println(string1);
+        StdOut.println(string2);
+    }
+}
