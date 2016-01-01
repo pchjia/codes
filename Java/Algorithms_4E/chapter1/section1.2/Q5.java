cab8a10fedd359b1d3eebc8fdd4ba68abd2efa28
pchjia <pchjia@sina.cn>
2016-01-01 09:18:58 +0800

    practise 1.2.5

---
+++ Java/Algorithms_4E/chapter1/section1.2/Q5.java
@@ -0,0 +1,8 @@
+public class Q5 {
+    public static void main (String[] args) {
+        String s = "hello world";
+        s.toUpperCase();
+        s.substring(6, 11);
+        StdOut.println(s); // hello world
+    }
+}
