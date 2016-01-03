5c643822e705ea5f5afc2f77c5a089cd5b8796ef
pchjia <pchjia@sina.cn>
2016-01-03 12:16:10 +0800

    practise 1.3.12

---
+++ Java/Algorithms_4E/chapter1/section1.3/Q12.java
@@ -0,0 +1,80 @@
+import java.util.Iterator;
+
+public class Q12 {}
+
+class Stack<Item> implements Iterable<Item> {
+    private Node first;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public static Stack<String> copy(Stack<String> stold) {
+        Stack<String> stnew = new Stack<String>();
+        Stack<String> sttmp = new Stack<String>();
+        for (String item: stold) {
+            sttmp.push(stold.pop());
+        }
+        for (String item: stnew) {
+            stnew.push(sttmp.pop());
+        }
+        return stnew;
+    }
+
+    public void push(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+        
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    public static void main (String[] args) {
+        Stack<String> s = new Stack<String>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) {
+                s.push(item);
+            } else if (!s.isEmpty()) {
+                StdOut.print(s.pop() + " ");
+            }
+        }
+        StdOut.println("(" + s.size() + " left on stack)");
+    }
+}
