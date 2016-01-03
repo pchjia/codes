98c3025a680c837314cae2115775db3523071f17
pchjia <pchjia@sina.cn>
2016-01-03 12:18:56 +0800

    practise 1.3.13

---
+++ Java/Algorithms_4E/chapter1/section1.3/Q13.java
@@ -0,0 +1,11 @@
+/*
+ * Queue:
+ * input: 
+ *  0~9
+ * output:
+ *  a. 0 1 2 3 4 5 6 7 8 9
+ *  b. 4 6 8 7 5 3 2 9 0 1 // impossible
+ *  c. 2 5 6 7 4 8 9 3 1 0 // impossible
+ *  d. 4 3 2 1 0 5 6 7 8 9 // impossible
+ */
+public class Q12 {}
