2e20c24b4043d8ad6b887dfe8f6be64e2c3f4bd8
pchjia <pchjia@sina.cn>
2016-01-03 21:26:16 +0800

    UF

---
+++ Java/Algorithms_4E/chapter1/UF.java
@@ -0,0 +1,107 @@
+public class UF {
+    private int[] id;
+    private int[] sz;
+    private int count;
+
+    public UF(int N) {
+        count = N;
+        id = new int[N];
+        sz = new int[N];
+        for (int i=0; i<N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /* first attempt: quick find
+    public int find(int p) {
+        return id[p];
+    }
+
+    public void union(int p, int q) {
+        int pID = find(p);
+        int qID = find(q);
+
+        if (pID != qID) {
+            return;
+        }
+
+        for (int i=0; i<id.length; i++) {
+            if (id[i] == pID) {
+                id[i] = qID;
+            }
+        }
+        
+        count --;
+    }
+    */
+
+    /* second attempt: quick-union
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) {
+            return;
+        }
+
+        id[pRoot] = qRoot;
+
+        count --;
+    }
+    */
+
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot) {
+            return;
+        }
+
+        if (sz[p] < sz[q]) {
+            id[p] = qRoot;
+            sz[q] += sz[p];
+        } else {
+            id[q] = pRoot;
+            sz[p] += sz[q];
+        }
+
+        count --;
+    }
+
+    public static void main (String[] args) {
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
+            }
+        }
+        StdOut.println(uf.count() + "components");
+    }
+}
