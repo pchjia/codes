5973ef6597aaf4c094d9d387a422fe17592822fb
pchjia <pchjia@sina.cn>
2016-01-03 10:47:31 +0800

    practise 1.3.8

---
+++ Java/Algorithms_4E/chapter1/section1.3/Q8.java
@@ -0,0 +1,10 @@
+/*
+ * DoublingStackOfStrings:
+ * input:
+ *      it was - the best - of time --- it was - the --
+ * output:
+ *      was best time of the was the it
+ * capacity:
+ *      4
+ */
+public class Q8 {}
