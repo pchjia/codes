070b50e7758dfa3eb2056d22badad34cf9dde397
pchjia <pchjia@sina.cn>
2016-01-03 11:22:21 +0800

    practise 1.3.9

---
+++ Java/Algorithms_4E/chapter1/section1.3/Q9.java
@@ -0,0 +1,39 @@
+public class Q9 {
+    public static void main (String[] args) {
+        Stack<String> input = new Stack<String>();
+        Stack<String> output = new Stack<String>();
+        Stack<String> outputVals = new Stack<String>();
+        Stack<String> outputOpts = new Stack<String>();
+        Stack<String> tmp = new Stack<String>();
+
+        while (!StdIn.isEmpty()) {
+            input.push(StdIn.readString());
+        }
+
+        for (String s: input) {
+            if (s.equals(")")) {
+                output.push(s);
+                outputOpts.push(")");
+            } else if (s.equals("+") || s.equals("-") || 
+                    s.equals("*") || s.equals("/")) {
+                outputOpts.push(s);
+                output.push(s);
+
+            } else {
+                output.push(s);
+                outputVals.push(s);
+                while (!outputOpts.isEmpty()) {
+                    String op = outputOpts.pop();
+                    if (op.equals(")")) {
+                        break;
+                    }
+                    outputVals.pop();
+                    output.push("(");
+                }
+            }
+        }
+        for (String s: output) {
+            StdOut.print(s + " ");
+        }
+    }
+}
