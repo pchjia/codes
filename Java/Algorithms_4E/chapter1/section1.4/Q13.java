fd66d42238edaf080d3ccc3ace00866f7d493539
pchjia <pchjia@sina.cn>
2016-01-04 11:28:34 +0800

    practise 1.4.13

---
+++ Java/Algorithms_4E/chapter1/section1.4/Q13.java
@@ -0,0 +1,11 @@
+/*
+ * a. Accumulator                       double double int   16 + 8 + 8 + 4 ~ 40
+ * b. Transaction                       String Date double  16 + 8 + 8 + 8 ~ 40
+ * c. FixedCapacityStackOfStrings       String[] int        16 + 8 + 4     ~ 32
+ * d. Point2D                           double duble        16 + 8 + 8     ~ 32
+ * e. Interval1D                        double double                      ~ 32
+ * f. Interval2D                        Interval1D Interval1D 16 + 8 + 8   ~ 32
+ * g. Double                            double              16 + 8         ~ 24
+ */
+
+public class Q13 }
