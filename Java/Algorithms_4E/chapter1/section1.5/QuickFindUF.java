908924792d1264a221080eb8de354eada7daa619
pchjia <pchjia@sina.cn>
2016-01-05 10:35:39 +0800

    QuickFindUF

---
+++ Java/Algorithms_4E/chapter1/section1.5/QuickFindUF.java
@@ -0,0 +1,58 @@
+public class QuickFindUF {
+    private int[] id;
+    private int[] sz;
+    private int count;
+
+    public QuickFindUF(int N) {
+        count = N;
+        id = new int[N];
+        sz = new int[N];
+        for (int i=0; i<N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        return id[p];
+    }
+
+    public void union(int p, int q) {
+        int pID = find(p);
+        int qID = find(q);
+
+        if (pID != qID) {
+            return;
+        }
+
+        for (int i=0; i<id.length; i++) {
+            if (id[i] == pID) {
+                id[i] = qID;
+            }
+        }
+        
+        count --;
+    }
+
+    public static void main (String[] args) {
+        int N = StdIn.readInt();
+        QuickFindUF uf = new QuickFindUF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
+            }
+        }
+        StdOut.println(uf.count() + " components");
+    }
+}
