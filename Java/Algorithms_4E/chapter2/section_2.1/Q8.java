82dd70d8484bc978458ce8824fbce931e7e07a5f
pchjia <pchjia@sina.cn>
2016-01-05 20:03:31 +0800

    practise 2.1.8

---
+++ Java/Algorithms_4E/chapter2/section_2.1/Q8.java
@@ -0,0 +1,8 @@
+/*
+ * suppose items have three values, then the worst Insertion.sort() runtime will be power-level.
+ *          as the queue: bigest bigest ... medium dedium ... smallest smallest... (size N)
+ *                          N/3                 N/3             N/3
+ *          then exchange times is N/3*N/3 + N/3*N/2. still power-level.
+ */
+
+public class Q8 {}
