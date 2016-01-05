961eab29c30675550d27a300b9654f494588a649
pchjia <pchjia@sina.cn>
2016-01-05 19:29:41 +0800

    SortCompare

--- Java/Algorithms_4E/chapter2/SortCompare.java
+++ Java/Algorithms_4E/chapter2/SortCompare.java
@@ -8,11 +8,11 @@ public class SortCompare {
         if (alg.equals("Selection")) {
             Selection.sort(a);
         }
-/*
+
         if (alg.equals("Shell")) {
             Shell.sort(a);
         }
-
+/*
         if (alg.equals("Merge")) {
             Merge.sort(a);
         }
