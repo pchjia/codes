e7c2c7d44f41eed088f682c9cb0dd6517418e984
pchjia <pchjia@sina.cn>
2016-01-08 11:40:34 +0800

    practise 2.3.9

---
+++ Java/Algorithms_4E/chapter2/section_2.3/Q9.java
@@ -0,0 +1,21 @@
+/*
+ * ➜  section_2.3 git:(master) $ java Q2 a b c a b c a b c a b c    
+ * 0   , 8   , 11   b a b a b a a b c c c c 
+ * 0   , 5   , 7    a a b a a b b b c c c c 
+ * 0   , 2   , 4    a a a b a b b b c c c c 
+ * 0   , 1   , 1    a a a b a b b b c c c c 
+ * 3   , 4   , 4    a a a a b b b b c c c c 
+ * 6   , 7   , 7    a a a a b b b b c c c c 
+ * 9   , 10  , 11   a a a a b b b b c c c c
+ *
+ * ➜  section_2.3 git:(master) $ java Q2 a b a b a b a b a b a b
+ * 0   , 4   , 11   a a a a a b b a b b b b 
+ * 0   , 2   , 3    a a a a a b b a b b b b 
+ * 0   , 1   , 1    a a a a a b b a b b b b 
+ * 5   , 9   , 11   a a a a a b b a b b b b 
+ * 5   , 7   , 8    a a a a a a b b b b b b 
+ * 5   , 5   , 6    a a a a a a b b b b b b 
+ * 10  , 11  , 11   a a a a a a b b b b b b
+ */
+
+public class Q9 {}
