4c0c8d55e87101d3b6e0685f2fdca03834b73e2e
pchjia <pchjia@sina.cn>
2016-01-10 22:43:35 +0800

    practise 2.4.9

---
+++ Java/Algorithms_4E/chapter2/section_2.4/Q9.java
@@ -0,0 +1,25 @@
+/*
+ * 1 : E
+ * 2 : C/D
+ * 3 : A/B/C/D
+ * 4 : A/B/C
+ * 5 : A/B
+ *  E D C B A
+ *  E D C A B
+ *   E D B A C
+ *   E D B C A
+ *    E D A B C
+ *    E D A C B
+ *  E C D B A
+ *  E C D A B
+ *
+ *  1 B
+ *  2 B A
+ *  3 B A
+ *  4 A
+ *  5 A
+ *  B B A A A
+ *  B A B A A
+ */
+
+public class Q9 {}
