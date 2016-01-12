78da9833f835aa42a320b1155b2397bf78f95633
pchjia <pchjia@sina.cn>
2016-01-12 22:44:41 +0800

    practise 2.4.17

---
+++ Java/Algorithms_4E/chapter2/section_2.4/Q17.java
@@ -0,0 +1,10 @@
+/*
+ * for array pq[] :
+ *  first:
+ *      1 2 3 4 5 ... k-1 k k+1 ... N
+ *  after delete k element:
+ *      every time, pq's minimal key will be removed, so there are still k k+1 ... N in the array;
+ *      the array is 1 2 3 4 5 ... k+1 ... N
+ */
+
+public class Q17 {}
