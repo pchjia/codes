ad94268933f695b6ef080502cac887a565516b28
pchjia <pchjia@sina.cn>
2016-01-12 22:34:47 +0800

    practise 2.4.16

---
+++ Java/Algorithms_4E/chapter2/section_2.4/Q16.java
@@ -0,0 +1,7 @@
+/*
+ * for Heap.sort():
+ *  when the array is ascdening, the function will take max exchange;
+ *  the same as above, when it is descending, it will take min exchange;
+ */
+
+public class Q16 {}
