29b25cbf4a24e50b27e2544324f9f85e56b87ddc
pchjia <pchjia@sina.cn>
2016-01-18 20:03:59 +0800

    delete virtualenv_2

--- Python/ScrapingWithPython/scraping2/scrap1.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen("https://www.pchjia.com/index.html")
-bs_obj = BeautifulSoup(html)
-title_list = bs_obj.findAll("a", {"class":"post-title-link"})
-for title in title_list:
-    print(title)
