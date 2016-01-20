90d862250b47dcd7afe49210375b137edef1d6a0
pchjia <pchjia@sina.cn>
2016-01-20 14:04:56 +0800

    optimize code

--- Python/ScrapingWiththeWeb/scripts/scraping_2.py
+++ Python/ScrapingWiththeWeb/scripts/scraping_2.py
@@ -2,8 +2,15 @@ from bs4 import BeautifulSoup
 
 from model import openurl
 
-html = openurl("https://www.pchjia.com/index.html")
-bs_obj = BeautifulSoup(html)
-title_list = bs_obj.findAll("a", {"class":"post-title-link"})
-for title in title_list:
-    print(title)
+def main():
+    html = openurl("https://www.pchjia.com/index.html")
+    if html:
+        bs_obj = BeautifulSoup(html)
+        title_list = bs_obj.findAll("a", {"class":"post-title-link"})
+        for title in title_list:
+            print(title)
+    else:
+        print("illegal url")
+
+if __name__ == "__main__":
+    main()
