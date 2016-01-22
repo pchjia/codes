7721a8832059fa026ec82559546b187309e10b4a
pchjia <pchjia@sina.cn>
2016-01-22 22:00:44 +0800

    children tags

--- Python/ScrapingWiththeWeb/scripts/scraping_2.py
+++ Python/ScrapingWiththeWeb/scripts/scraping_2.py
@@ -6,8 +6,9 @@ def main():
     html = openurl("https://www.pchjia.com/index.html")
     if html:
         bs_obj = BeautifulSoup(html)
-        title_list = bs_obj.findAll("a", {"class":"post-title-link"})
+        title_list = bs_obj.find("", {"id":"posts"}).children
         for title in title_list:
+            print();
             print(title)
     else:
         print("illegal url")
