89b728524170929b20bb16c23685316ae2a4e6ec
pchjia <pchjia@sina.cn>
2016-01-23 17:17:32 +0800

    siblings

--- Python/ScrapingWiththeWeb/scripts/scraping_2.py
+++ Python/ScrapingWiththeWeb/scripts/scraping_2.py
@@ -6,9 +6,8 @@ def main():
     html = openurl("https://www.pchjia.com/index.html")
     bs_obj = BeautifulSoup(html)
     if html:
-        title_list = bs_obj.find("div", {"class":"post-body"}).children
+        title_list = bs_obj.find("div", {"class":"post-body"}).ol.li.next_siblings
         for title in title_list:
-            print();
             print(title)
     else:
         print("illegal url")
