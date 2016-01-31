a07e93db416bd2f383574d266d8301fc98ab566f
pchjia <pchjia@sina.cn>
2016-01-31 21:36:58 +0800

    practise 2.5.1

---
+++ Java/Algorithms_4E/chapter2/section_2.5/Q1.java
@@ -0,0 +1,13 @@
+public class Q1 {
+    public static void main(String[] args){
+
+    }
+
+    public static int compareTo(String that) {
+        // keep stable
+        if (this == that) {
+            return 0;
+        }
+        // ...
+    }
+}
