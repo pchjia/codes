9098c0f1f12ee018b9409ea12f52169b23b0b56e
pchjia <pchjia@sina.cn>
2016-01-31 22:50:27 +0800

    practise 2.5.5

---
+++ Java/Algorithms_4E/chapter2/section_2.5/Q5.java
@@ -0,0 +1,10 @@
+/*
+ * why Selection.sort() is not stable?
+ *      because the sort() function uses
+ *          if (less(a[i], a[min])) {
+ *              min = i;
+ *          }
+ */
+public class Q5 {
+
+}
