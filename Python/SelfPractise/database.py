586bfad4019ef5e35fe2e06b8c93fede366e1a69
pchjia <pchjia@sina.cn>
2016-02-03 23:22:18 +0800

    mysql db connection test

--- Python/SelfPractise/database.py
+++ Python/SelfPractise/database.py
@@ -1,21 +1,22 @@
-#!/bin/e/env python
+#!/bin/env python3
 #coding: utf-8
 
-import MySQLdb
+import mysql.connector # MySQLdb for python2
 
 def main():
     try:
-        con = MySQLdb.connect(user="pchjia", password="jia9692", database="test")
+        con = mysql.connector.connect(user="pchjia", password="jia9692", database="test")
         cur = con.cursor()
 
-        cur.execute("select 3*4")
+        cur.execute("select * from goods;")
 
-        data = cur.fetchone()
+        data = cur.fetchall()
 
         for row in data:
             print(row)
 
-    except MySQLdb.error:
+    except :
+        print("error")
         if con:
             con.rollback()
             print("SQL error")
@@ -23,3 +24,6 @@ def main():
     finally:
         if con:
             con.close()
+
+if __name__ == "__main__":
+    main()
