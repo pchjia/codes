ab68d3454996a5832394afcf2bc54d6b946c7445
pchjia <pchjia@sina.cn>
2016-02-03 22:17:18 +0800

    fix url

--- Python/SelfPractise/movie.py
+++ Python/SelfPractise/movie.py
@@ -7,13 +7,14 @@ urls = ['http://www.dytt8.net', 'http://www.dygod.net',
         'http://www.dy2018.com', 'http://www.dytt.net']
 
 def find_movies(url, fav):
-    headers = {'Host': url.split(r"//")[1],
-            'Connection': 'keep-alive',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    host = url.split(r"//")[1]
+    headers = {'Host': host,
+#            'Connection': 'keep-alive',
+#            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-            'Referer': 'https://www.google.com/',
-            'Accept-Encoding': 'gzip, deflate, sdch',
-            'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
+#            'Referer': 'https://www.google.com/',
+#            'Accept-Encoding': 'gzip, deflate, sdch',
+#            'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
             }
 
     s = requests.Session()
@@ -25,7 +26,8 @@ def find_movies(url, fav):
     for link in links:
         for f in fav:
             if f in link[1]:
-                print(link)
+                url = re.findall(r'href=[\"|\'](.*?)[\"|\']', link[0])
+                print(host + url[0] + "\t\t" + link[1])
 
 #fav = [u'科幻', u'动作', u'动画', u'高分']
 fav = [u'科幻']
